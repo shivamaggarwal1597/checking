647e85d64037e408e7d4cf4ccb1ebf70b97ec2b8
ashish aggarwal <ashishaggarwal@ashishs-MacBook-Air.local>
2017-07-23 15:21:00 +0530

    Initial Commit

---
+++ nb/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nb
+//
+//  Created by ashish aggarwal on 23/07/17.
+//  Copyright © 2017 ashish aggarwal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
